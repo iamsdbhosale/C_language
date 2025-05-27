83c7394fb7b584bbda2f0d5a061144a99404bdf9
iamsdbhosale <sdbhosale@gmail.com>
2025-05-27 11:47:04 +0530

    updated

--- 1.c
+++ 1.c
@@ -7,3 +7,14 @@ void main()
 {
 printf("\n Welcome \n");
 }
+
+/*
+sdbhosale@humanrobot:~/C_language$ ls
+1.c  a.out
+sdbhosale@humanrobot:~/C_language$ gcc 1.c -o one
+sdbhosale@humanrobot:~/C_language$ ls
+1.c  a.out  one
+sdbhosale@humanrobot:~/C_language$ ./one
+
+ Welcome
+*/
