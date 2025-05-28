0e225ab0d10391da793a9701c987ce64b8ccdbe5
iamsdbhosale <sdbhosale@gmail.com>
2025-05-28 12:47:58 +0530

    snake.cpp added

---
+++ games/snake.cpp
@@ -0,0 +1,191 @@
+#include <iostream>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+using namespace std;
+
+int _kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+
+char _getch() {
+    struct termios oldt, newt;
+    char ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
+/*
+#include <iostream>
+#include <conio.h>    // For _kbhit() and _getch()
+#include <windows.h>  // For Sleep()
+using namespace std;
+*/
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
+int speed = 100; // Default speed
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+    nTail = 0;
+
+    int choice;
+    cout << "Select Speed Level:\n";
+    cout << "1. Slow\n";
+    cout << "2. Medium\n";
+    cout << "3. Fast\n";
+    cout << "Enter choice (1-3): ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1: speed = 200; break;
+        case 2: speed = 100; break;
+        case 3: speed = 50; break;
+        default: cout << "Invalid choice. Defaulting to Medium.\n"; speed = 100;
+    }
+
+    cout << "Use W A S D to move. Press X to exit.\n";
+    //Sleep(1500); // Wait before game starts
+      usleep(1500 * 1000);  // Convert milliseconds to microseconds
+}
+
+void Draw() {
+//    system("cls"); // Clear screen
+    cout << "\033[2J\033[1;1H";  // Clear screen and move cursor to top-left
+
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0) cout << "#"; // Left wall
+
+            if (i == y && j == x)
+                cout << "O"; // Snake head
+            else if (i == fruitY && j == fruitX)
+                cout << "F"; // Fruit
+            else {
+                bool print = false;
+                for (int k = 0; k < nTail; k++) {
+                    if (tailX[k] == j && tailY[k] == i) {
+                        cout << "o"; // Snake tail
+                        print = true;
+                    }
+                }
+                if (!print) cout << " ";
+            }
+
+            if (j == width - 1) cout << "#"; // Right wall
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++) cout << "#";
+    cout << endl;
+
+    cout << "Score: " << score << endl;
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+            case 'a': dir = LEFT; break;
+            case 'd': dir = RIGHT; break;
+            case 'w': dir = UP; break;
+            case 's': dir = DOWN; break;
+            case 'x': gameOver = true; break;
+        }
+    }
+}
+
+void Logic() {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++) {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir) {
+        case LEFT:  x--; break;
+        case RIGHT: x++; break;
+        case UP:    y--; break;
+        case DOWN:  y++; break;
+        default: break;
+    }
+
+    // Collision with wall
+    if (x >= width || x < 0 || y >= height || y < 0)
+        gameOver = true;
+
+    // Collision with tail
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    // Eat fruit
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+        //Sleep(speed); // Control game speed
+        usleep(speed * 1000);  // Convert milliseconds to microseconds
+
+    }
+    cout << "Game Over! Final Score: " << score << endl;
+    return 0;
+}
+
